467b4e654e17bbf785641fe572347b8356fc3854
muhammed <muhammed.mammadhuseynov@gmail.com>
2023-11-07 14:31:56 +0400

    fix minor changes

--- Monefy/Views/HomeView.xaml.cs
+++ Monefy/Views/HomeView.xaml.cs
@@ -21,5 +21,24 @@ namespace Monefy.Views
         {
             InitializeComponent();
         }
+        private void ScrollViewer_PreviewMouseWheel(object sender, MouseWheelEventArgs e)
+        {
+            if (sender is ScrollViewer scrollViewer)
+            {
+                double deltaY = -e.Delta;
+
+                if (e.Delta > 0)
+                {
+                    scrollViewer.LineUp();
+                }
+                else if (e.Delta < 0)
+                {
+                    scrollViewer.LineDown();
+                }
+
+                e.Handled = true;
+            }
+        }
+
     }
 }
