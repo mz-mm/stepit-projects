d8af04783df214a966052b4f021cd13e78321beb
mz-mm <muhammed.mammadhuseynov@gmail.com>
2024-02-04 07:56:32 +0400

    commit

--- WarehouseMS/WarehouseMS.Presentation/Views/MainView.xaml.cs
+++ WarehouseMS/WarehouseMS.Presentation/Views/MainView.xaml.cs
@@ -1,4 +1,5 @@
 ﻿using System.Windows;
+using System.Windows.Input;
 
 namespace WarehouseMS.Presentation.Views;
 
@@ -7,5 +8,21 @@ public partial class MainView : Window
     public MainView()
     {
         InitializeComponent();
+    }
+
+    private void Minimize_Click(object sender, RoutedEventArgs e)
+    {
+        WindowState = WindowState.Minimized;
+
+    }
+
+    private void Close_Click(object sender, RoutedEventArgs e)
+    {
+        App.Current.Shutdown();
+    }
+
+    private void Border_MouseDown(object sender, MouseButtonEventArgs e)
+    {
+        this.DragMove();
     }
 }
