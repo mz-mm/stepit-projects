e00ec030ba358603092e5a152dc8aab4a39730d5
Маммадгусейнов Мухаммед Заур <Mamma_ms85@itstep.edu.az>
2024-02-13 11:50:29 +0400

    commit

--- WarehouseMS/WarehouseMS.Presentation/ViewModels/ProductsViewModel.cs
+++ WarehouseMS/WarehouseMS.Presentation/ViewModels/ProductsViewModel.cs
@@ -1,10 +1,20 @@
-﻿using GalaSoft.MvvmLight;
-using GalaSoft.MvvmLight.Messaging;
+﻿using System.Collections.ObjectModel;
+using GalaSoft.MvvmLight;
+using WarehouseMS.Domain.Dtos.ProductDtos;
 
 namespace WarehouseMS.Presentation.ViewModels;
 
 public class ProductsViewModel : ViewModelBase
 {
+
+    private ObservableCollection<GetProductDto> _products;
+    public ObservableCollection<GetProductDto> product
+    {
+        get => _products;
+        set => Set(ref _products, value);
+    }
+
+    
     public ProductsViewModel()
     {
     }
