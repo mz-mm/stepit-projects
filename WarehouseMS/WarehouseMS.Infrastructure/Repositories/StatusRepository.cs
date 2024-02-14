d5993d89bfd27e0a2b3d1e4d80ef0574ab675166
mz-mm <muhammed.mammadhuseynov@gmail.com>
2024-02-14 11:59:33 +0400

    Add status repository

---
+++ WarehouseMS/WarehouseMS.Infrastructure/Repositories/StatusRepository.cs
@@ -0,0 +1,12 @@
+﻿using WarehouseMS.Infrastructure.Context;
+using WarehouseMS.Infrastructure.Context.Entities;
+using WarehouseMS.Infrastructure.Interfaces;
+
+namespace WarehouseMS.Infrastructure.Repositories;
+
+public class StatusRepository : Repository<Status>, IStatusRepository
+{
+    public StatusRepository(AppDbContext context) : base(context)
+    {
+    }
+}
